a4dbfa0e005862f31502a5bc4acfff8a74e6ce57
Ishpreet Kaur Khalsa <113664759+ishpreetkaurkhalsa@users.noreply.github.com>
2024-08-09 01:54:11 +0530

    frontend

---
+++ src/components/AppBar.tsx
@@ -0,0 +1,15 @@
+function AppBar() {
+  return (
+    <div className="border-[#25262B] bg-black border-b-2 fixed text-white h-16 w-screen flex items-center justify-center">
+      <div className="">
+        <img
+          src="logo.svg"
+          alt="logo"
+          className=" h-6"
+        ></img>
+      </div>
+    </div>
+  );
+}
+
+export default AppBar;
